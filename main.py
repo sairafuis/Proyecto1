6ed1adc61cbb4dbf10ab57fb6b0c3db976cbf6f3
sairafuis <30506369+sairafuis@users.noreply.github.com>
2024-11-17 15:38:49 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,110 @@
+import gra
+
+ga1 = gra.Grafo(False).dorogov(100)
+gra.Grafo(False).saveGraph(ga1)
+ga2 = gra.Grafo(False).dijkstra(50,ga1)
+gra.Grafo(False).saveGraph2(ga1,ga2)
+#ga2 = gra.Grafo(False).BFS(3,ga1)
+#gra.Grafo(False).saveGraph(ga2)
+#revisado = set()
+#a = []
+#ga2 = gra.Grafo(False).DFSRecursive(3,ga1,revisado,a)
+#gra.Grafo(False).saveGraph(ga2)
+#ga2 = gra.Grafo(False).DFSIterative(3,ga1)
+#gra.Grafo(False).saveGraph(ga2)
+
+#ga1 = gra.Grafo(False).dorogov(100)
+#gra.Grafo(False).saveGraph(ga1)
+
+#ga1 = gra.Grafo(False).dorogov(500)
+#gra.Grafo(False).saveGraph(ga1)
+
+#ga1 = gra.Grafo(False).grafoMalla(25,25)
+#gra.Grafo(False).saveGraph(ga1)
+#ga2 = gra.Grafo(False).BFS(3,ga1)
+#gra.Grafo(False).saveGraph(ga2)
+#revisado = set()
+#a = []
+#ga2 = gra.Grafo(False).DFSRecursive(3,ga1,revisado,a)
+#gra.Grafo(False).saveGraph(ga2)
+#ga2 = gra.Grafo(False).DFSIterative(3,ga1)
+#gra.Grafo(False).saveGraph(ga2)
+
+#ga1 = gra.Grafo(False).grafoMalla(5,5)
+#gra.Grafo(False).saveGraph(ga1)
+#ga2 = gra.Grafo(False).dijkstra(10,ga1)
+#gra.Grafo(False).saveGraph2(ga1,ga2)
+
+#ga1 = gra.Grafo(False).grafoMalla(25,25)
+#gra.Grafo(False).saveGraph(ga1)
+
+#ga1 = gra.Grafo(False).barasi(150,3)
+#gra.Grafo(False).saveGraph(ga1)
+#ga2 = gra.Grafo(False).dijkstra(30,ga1)
+#gra.Grafo(False).saveGraph2(ga1,ga2)
+#ga2 = gra.Grafo(False).BFS(3,ga1)
+#gra.Grafo(False).saveGraph(ga2)
+#revisado = set()
+#a = []
+#ga2 = gra.Grafo(False).DFSRecursive(3,ga1,revisado,a)
+#gra.Grafo(False).saveGraph(ga2)
+#ga2 = gra.Grafo(False).DFSIterative(3,ga1)
+#gra.Grafo(False).saveGraph(ga2)
+
+#ga1 = gra.Grafo(False).barasi(100,5)
+#gra.Grafo(False).saveGraph(ga1)
+
+#ga1 = gra.Grafo(False).barasi(500,5)
+#gra.Grafo(False).saveGraph(ga1)
+
+#ga1 = gra.Grafo(False).geografica(50,.3)
+#gra.Grafo(False).saveGraph(ga1)
+#ga2 = gra.Grafo(False).dijkstra(25,ga1)
+#gra.Grafo(False).saveGraph2(ga1,ga2)
+#ga2 = gra.Grafo(False).BFS(10,ga1)
+#gra.Grafo(False).saveGraph(ga2)
+#revisado = set()
+#a = []
+#ga2 = gra.Grafo(False).DFSRecursive(3,ga1,revisado,a)
+#gra.Grafo(False).saveGraph(ga2)
+#ga2 = gra.Grafo(False).DFSIterative(3,ga1)
+#gra.Grafo(False).saveGraph(ga2)
+
+
+#ga1 = gra.Grafo(False).geografica(100,.7)
+#gra.Grafo(False).saveGraph(ga1)
+
+#ga1 = gra.Grafo(False).geografica(500,.7)
+#gra.Grafo(False).saveGraph(ga1)
+
+#ga1 = gra.Grafo(False).gilbert(100,.7)
+#gra.Grafo(False).saveGraph(ga1)
+#ga2 = gra.Grafo(False).dijkstra(50,ga1)
+#gra.Grafo(False).saveGraph2(ga1,ga2)
+#ga2 = gra.Grafo(False).BFS(10,ga1)
+#gra.Grafo(False).saveGraph(ga2)
+#revisado = set()
+#a = []
+#ga2 = gra.Grafo(False).DFSRecursive(3,ga1,revisado,a)
+#gra.Grafo(False).saveGraph(ga2)
+#ga2 = gra.Grafo(False).DFSIterative(3,ga1)
+#gra.Grafo(False).saveGraph(ga2)
+
+#ga1 = gra.Grafo(False).gilbert(100,.5)
+#gra.Grafo(False).saveGraph(ga1)
+
+#ga1 = gra.Grafo(False).gilbert(500,.5)
+#gra.Grafo(False).saveGraph(ga1)
+
+#ga1 = gra.Grafo(False).erdosRenyi(200,1200)
+#gra.Grafo(False).saveGraph(ga1)
+#ga2 = gra.Grafo(False).dijkstra(100,ga1)
+#gra.Grafo(False).saveGraph2(ga1,ga2)
+#ga2 = gra.Grafo(False).BFS(10,ga1)
+#gra.Grafo(False).saveGraph(ga2)
+#revisado = set()
+#a = []
+#ga2 = gra.Grafo(False).DFSRecursive(3,ga1,revisado,a)
+#gra.Grafo(False).saveGraph(ga2)
+#ga2 = gra.Grafo(False).DFSIterative(3,ga1)
+#gra.Grafo(False).saveGraph(ga2)
